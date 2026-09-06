7cda16c177fed95cd4cd009bdd8171c6faf44b58
Josip Kilić <neksuses@gmail.com>
2026-09-06 14:05:23 +0200

    Work on colors

---
+++ lib/theme/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+abstract class BokunSpizeColors {
+  ///
+  /// LIGHT THEME
+  ///
+
+  static const lightThemeText = Color(0xFF121412);
+  static const lightThemeBackground = Color(0xFFFFFFFF);
+  static const lightThemeScaffold = Color(0xFFDCE0D9);
+
+  ///
+  /// PRIMARY COLORS
+  ///
+
+  static const green = Color(0xFF3B7D5E);
+  static const blue = Color(0xFF4F5094);
+  static const bordeaux = Color(0xFF885A89);
+  static const red = Color(0xFF9D5C63);
+}
